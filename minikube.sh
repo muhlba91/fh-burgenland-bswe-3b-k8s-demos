3d261149406e1d5cb84b399af8bcb59776c7ef45
Daniel Muehlbachler-Pietrzykowski <daniel@muehlbachler.io>
2023-12-02 11:46:32 +0100

    feat: change name

--- minikube.sh
+++ minikube.sh
@@ -3,7 +3,7 @@ set -eou pipefail
 
 # configure minikube for your environment first!
 
-minikube start # minikube start --memory 6144 --cpus 2
+minikube start # minikube start --memory 6144 --cpus 2 --driver=podman --container-runtime=containerd
 minikube addons enable csi-hostpath-driver
 minikube addons enable metrics-server
 
